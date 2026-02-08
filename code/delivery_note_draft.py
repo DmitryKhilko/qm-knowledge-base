e101d604202e04505d4ebce23a7350080678fd90
DmitryKhilko <dmitrykhilko@mail.ru>
2026-02-09 00:42:23 +0300

    Первый commit

---
+++ code/delivery_note_draft.py
@@ -0,0 +1,46 @@
+import delivery_note 
+
+def delivery_note_draft_private_create():
+    """
+    Назначение: 
+    Создание приватного черновика накладной.
+
+    Требования:
+    - спецификация: SPEC-FEATURE-DELIVERY-NOTE-DRAFTS
+    - требования: FR-DELIVERY-NOTE-DRAFTS-CREATE-001 
+    """
+    pass
+    
+def delivery_note_draft_private_read():
+    """
+    Назначение: 
+    Вывод списка приватных накладных.
+
+    Требования:
+    - спецификация: SPEC-FEATURE-DELIVERY-NOTE-DRAFTS
+    - требования: FR-DELIVERY-NOTE-DRAFTS-CREATE-001 
+    """
+    pass
+
+def delivery_note_draft_publish_read():
+    """
+    Назначение: 
+    Вывод списка публичных накладных.
+
+    Требования:
+    - спецификация: SPEC-FEATURE-DELIVERY-NOTE-DRAFTS
+    - требования: FR-DELIVERY-NOTE-DRAFTS-CREATE-001 
+    """
+    pass
+
+def delivery_note_draft_update():
+    """
+    Назначение: 
+    Обновление приватного черновика накладной.
+
+    Требования:
+    - спецификация: SPEC-FEATURE-DELIVERY-NOTE-DRAFTS
+    - требования: FR-DELIVERY-NOTE-DRAFTS-CREATE-001 
+    """
+    pass
+
